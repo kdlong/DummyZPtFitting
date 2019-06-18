9112b872a400157c14aa473024701f0eddf63739
Kenneth Long <kdlong@hep.wisc.edu>
2019-06-18 12:43:45 -0400

    Switch to pt(Z)

--- makeDummyFile.py
+++ makeDummyFile.py
@@ -2,19 +2,25 @@
 import ROOT
 import numpy 
 
-infile = ROOT.TFile("/afs/cern.ch/user/k/kelong/work/WZAnalysis/CMSSW_10_6_0_patch1/src/Analysis/VVAnalysis/testZ.root")
+infile = ROOT.TFile("/afs/cern.ch/user/k/kelong/work/WZAnalysis/CMSSW_10_6_0_patch1/src/Analysis/VVAnalysis/Zstudy_withScaleFacs.root")
 infile.ls()
 outfile = ROOT.TFile("fitPtZlep1.root", "recreate")
 dydir = outfile.mkdir("DY")
 dydir.cd()
-inhist = infile.Get("DYm50/ptl1_mm")
-hist = ROOT.TH1D("ptl1_mm", "ptl1_mm", 80, 0, 80)
-for i in range(hist.GetNbinsX()+1):
-    hist.SetBinContent(i, inhist.GetBinContent(i))
+#var = "ptl1"
+var = "ZPt_mm"
+inhist = infile.Get("DYm50/%s" % var)
+sumweights = infile.Get("DYm50/sumweights").Integral()
+inhist.Scale(35.9*1000*5765.4/sumweights)
+#hist = ROOT.TH1D("ptl1_mm", "ptl1_mm", 80, 25, 80)
+#print inhist.FindBin(25)
+#for i in range(26, hist.GetNbinsX()+26):
+#    hist.SetBinContent(i-25, inhist.GetBinContent(i))
+hist = inhist.Clone()
 
 errhistUp = hist.Clone(hist.GetName()+"_dummyErrUp")
 errhistDown = hist.Clone(hist.GetName()+"_dummyErrDown")
-for i in range(hist.GetNbinsX()+1):
+for i in range(1, hist.GetNbinsX()+1):
     errhistDown.SetBinContent(i, hist.GetBinContent(i)*.95)
     errhistUp.SetBinContent(i, hist.GetBinContent(i)*1.05)
 dydir.Write()
@@ -22,14 +28,14 @@ dydir.Write()
 bkdir = outfile.mkdir("bkgd")
 bkdir.cd()
 bkhist = hist.Clone()
-for i in range(hist.GetNbinsX()+1):
+for i in range(1, hist.GetNbinsX()+1):
     bkhist.SetBinContent(i, hist.GetBinContent(i)*(hist.GetNbinsX()+1-i)*0.0005)
 bkdir.Write()
 
 datadir = outfile.mkdir("data_obs")
 datadir.cd()
 datahist = hist.Clone()
-for i in range(hist.GetNbinsX()+1):
+for i in range(1, hist.GetNbinsX()+1):
     rate = hist.GetBinContent(i) + bkhist.GetBinContent(i)
     smeared_rate = numpy.random.normal(rate, rate*0.03)
 
